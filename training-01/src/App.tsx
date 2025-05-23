4363a3570f60d28c35635fc3295a5bace26b4b90
Kanaya-r <kanaya.r@nadia.bz>
2025-05-23 15:52:57 +0900

    [add] タスク追加機能の実装

---
+++ training-01/src/App.tsx
@@ -0,0 +1,64 @@
+/* TODO 追加／完了機能に加え、All | Active | Completed のタブで表示を切り替えられるようにする。 */
+import { useReducer } from 'react'
+import Input from './components/Input'
+import './App.css'
+
+function App() {
+  type Todo = {
+    id: number;
+    status: 'active' | 'completed';
+    text: string;
+  };
+
+  type Action = { type: 'add'; text: string };
+
+  const todoReducer = (state: Todo[], action: Action): Todo[] => {
+    switch ( action.type ) {
+      case 'add':
+        return [
+          ...state,
+          {
+            id: Date.now(),
+            status: 'active',
+            text: action.text
+          }
+        ];
+      default:
+        return state;
+    }
+  }
+
+  const initialTodos: Todo[] = [
+    {
+      id: 0,
+      status: 'active',
+      text: 'todo1'
+    }
+  ];
+
+  const [ todos, dispatch ] = useReducer( todoReducer, initialTodos );
+
+  const addTodo = (text: string) => {
+    dispatch({ type: 'add', text });
+  }
+
+
+  return (
+    <>
+      <Input addTodo={ addTodo } />
+      <ul>
+        { todos.map((todo: Todo) => {
+          return (
+            <li key={todo.id}>
+              <input type="checkbox" />
+              {todo.text}
+            </li>
+          )
+        })}
+      </ul>
+      <button type="button">完了</button>
+    </>
+  )
+}
+
+export default App
---
+++ training-01/src/components/Input.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+
+type InputProps = {
+  addTodo: (value: string) => void;
+};
+
+const Input = ({ addTodo }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submit = () => {
+    const value = inputRef.current?.value || '';
+    if (value.trim()) {
+      addTodo(value);
+      if (inputRef.current) {
+        inputRef.current.value = '';
+      }
+    }
+  }
+
+  return (
+    <>
+      <input type="text" ref={inputRef}/>
+      <button type="button" onClick={ submit }>追加</button>
+    </>
+  )
+}
+
+export default Input;
