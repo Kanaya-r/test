9c52b850bf92502221dba2c7a8eb0093cb90bf38
Kanaya-r <kanaya.r@nadia.bz>
2025-08-20 08:42:02 +0900

    [update] 目的地調整

--- trainig-02/src/assets/ts/spots.ts
+++ trainig-02/src/assets/ts/spots.ts
@@ -5,22 +5,22 @@ const RADIUS_M_LV1 = 100
 export const SPOTS: StampSpot[] = [
   {
     id: 's1',
-    name: '拓勇公園',
-    location: { lat: 42.67277145098463, lng: 141.67365122514187 },
+    name: 'キラキラ公園',
+    location: { lat: 42.64329954343844, lng: 141.62239023844242 },
     radiusM: RADIUS_M_LV1,
     description: 'STEP1'
   },
   {
     id: 's2',
     name: '苫小牧駅',
-    location: { lat: 42.63969517343846, lng: 141.5959744521637 },
+    location: { lat: 42.63988458923986, lng: 141.59698837178598 },
     radiusM: RADIUS_M_LV1,
     description: 'STEP2'
   },
   {
     id: 's3',
     name: 'ふるさと海岸',
-    location: { lat: 42.62851848658491, lng: 141.61288309768256 },
+    location: { lat: 42.627665929617685, lng: 141.6099380312791 },
     radiusM: RADIUS_M_LV1,
     description: 'STEP3'
   }
