77fb8282ee75e0b6144b8b0b51c99a471d4a531b
Kanaya-r <kanaya.r@nadia.bz>
2025-08-20 08:53:29 +0900

    [update] 見出しコンポーネントのexport方法変更

--- trainig-02/src/components/HeroTitle.tsx
+++ trainig-02/src/components/HeroTitle.tsx
@@ -1,5 +1,7 @@
 import type { childrenText } from "../assets/ts/types"
 
-export function H1({ children }: childrenText) {
+function H1({ children }: childrenText) {
   return <h1 className="text-3xl text-center font-extrabold tracking-tight text-slate-900 drop-shadow-sm">{ children }</h1>
 }
+
+export { H1 }
