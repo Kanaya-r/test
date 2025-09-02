c79599a440a60943caa32f726690750827f8123e
Kanaya-r <kanaya.r@nadia.bz>
2025-09-02 22:56:01 +0900

    [add] ヘッダーコンポーネント作成
    isActive関数でカレント判定。aria-currentの'page'は、WAI-ARIA仕様で「現在のページ」を意味しするため、スクリーンリーダーなどが「今いるページ」と認識できるようになる。

--- training-03/src/app/layout.tsx
+++ training-03/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+
+import Header from '../components/Header/Header'
+
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
 import "../styles/globals.scss"
 
 const geistSans = Geist({
@@ -25,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
