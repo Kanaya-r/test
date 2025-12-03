0a2e5e063606db56e7e8685ad3cb5e2d8682c454
Kanaya-r <kanaya.r@nadia.bz>
2025-12-03 20:19:47 +0900

    [remove] リポジトリ移行

--- trainig-02/src/components/HeroTitle.tsx
+++
@@ -1,7 +0,0 @@
-import type { ChildrenText } from "../assets/ts/types"
-
-function H1({ children }: ChildrenText) {
-  return <h1 className="text-3xl text-center font-extrabold tracking-tight text-slate-900 drop-shadow-sm">{ children }</h1>
-}
-
-export { H1 }
